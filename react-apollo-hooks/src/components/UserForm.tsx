ef197fe0856c28ae99159afbb99d81b0ea0245e5
Christopher Bartling <chris.bartling@gmail.com>
2019-08-14 01:25:09 -0500

    Styling with Bootstrap.

--- react-apollo-hooks/src/components/UserForm.tsx
+++ react-apollo-hooks/src/components/UserForm.tsx
@@ -5,7 +5,7 @@ import usersQuery from "../graphql/queries/UsersQuery";
 
 const UserForm: React.FC = () => {
 
-    const [insertMutation] = useMutation(createNewUserMutation, {
+    const [insertMutation, {loading, error}] = useMutation(createNewUserMutation, {
         update: (cache, {data: {insert_users: {returning}}}) => {
             // @ts-ignore
             const {users} = cache.readQuery({query: usersQuery});
@@ -34,26 +34,44 @@ const UserForm: React.FC = () => {
         });
     };
 
+    const renderLoadingMessage = () => {
+        return (
+            <div className="alert alert-info">
+                Saving new user...
+            </div>
+        );
+    };
+
+    const renderErrorMessage = () => {
+        return (
+            <div className="alert alert-danger">
+                An error occurred! Unable to save the user.
+            </div>
+        );
+    };
+
     return (
-        <div>
+        <div className="container">
             <h1>User Form</h1>
             <div>
                 <form onSubmit={async (event) => handleSubmit(event)}>
                     <div>
                         <label> First name
-                            <input type="text" name="firstName" ref={firstNameRef}/>
+                            <input type="text" name="firstName" ref={firstNameRef} className="form-control"/>
                         </label>
                     </div>
                     <div>
                         <label> Last name
-                            <input type="text" name="lastName" ref={lastNameRef}/>
+                            <input type="text" name="lastName" ref={lastNameRef} className="form-control"/>
                         </label>
                     </div>
                     <div>
-                        <button type="submit">Save</button>
+                        <button type="submit" className="btn btn-lg btn-primary">Save</button>
                     </div>
                 </form>
             </div>
+            {loading && renderLoadingMessage()}
+            {error && renderErrorMessage()}
         </div>
     );
 };
