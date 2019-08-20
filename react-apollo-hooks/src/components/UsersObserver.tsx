cf33f26a7a5f5bf24df65c978c485b2a70b1eadf
Christopher Bartling <chris.bartling@gmail.com>
2019-08-19 20:42:20 -0500

    Writing to the console when the subscription event comes in.

--- react-apollo-hooks/src/components/UsersObserver.tsx
+++ react-apollo-hooks/src/components/UsersObserver.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import {useSubscription} from "@apollo/react-hooks";
 import usersSubscription from "../graphql/subscriptions/UsersSubscription";
-import InfoAlert from "./InfoAlert";
 
 const UsersObserver: React.FC = () => {
     const {data, loading} = useSubscription(usersSubscription, {});
 
     if (!loading && data) {
-        // @ts-ignore
-        return <InfoAlert/>;
-    } else {
-        return null;
+        console.info("Received notification through GraphQL subscription.", data);
     }
+    return null;
 };
 
 export default UsersObserver;
